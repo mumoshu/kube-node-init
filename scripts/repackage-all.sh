f4af6896bd4c907108a1c98b515abbbbc9fd73e1
Yusuke Kuoka <ykuoka@gmail.com>
2019-10-04 07:44:58 +0900

    try to fix repackage-all

--- scripts/repackage-all.sh
+++ scripts/repackage-all.sh
@@ -6,6 +6,7 @@ rm -rf packages/ || true
 mkdir -p packages/
 for v in 0.1.0 0.1.1 0.1.2 0.2.0 0.2.1; do
   git checkout kube-node-init-$v
-  helm package -u charts/kube-node-init --destination packages
+  helm dep build charts/kube-node-init
+  helm package charts/kube-node-init --destination packages
 done
 git checkout master
