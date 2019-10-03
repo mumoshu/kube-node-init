ff8b412352e493a7eb1d1d996548e895821b8bf2
Yusuke Kuoka <ykuoka@gmail.com>
2019-10-04 07:38:39 +0900

    fix "Error: found in requirements.yaml, but missing in charts/ directory: common"

--- scripts/repackage-all.sh
+++ scripts/repackage-all.sh
@@ -6,6 +6,6 @@ rm -rf packages/ || true
 mkdir -p packages/
 for v in 0.1.0 0.1.1 0.1.2 0.2.0 0.2.1; do
   git checkout kube-node-init-$v
-  helm package charts/kube-node-init --destination packages
+  helm package -u charts/kube-node-init --destination packages
 done
 git checkout master
