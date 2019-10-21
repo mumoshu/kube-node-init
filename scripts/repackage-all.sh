eedbb52b4365fd8c4a9e5494240cc64721f8f4d7
Yusuke Kuoka <ykuoka@gmail.com>
2019-10-21 11:27:12 +0900

    fix repackage-all.sh to succeed

--- scripts/repackage-all.sh
+++ scripts/repackage-all.sh
@@ -4,9 +4,18 @@ set -e
 
 rm -rf packages/ || true
 mkdir -p packages/
-for v in 0.1.0 0.1.1 0.1.2 0.2.0 0.2.1; do
+
+# 0.1.0 is unable to be built on some reason. just skip it assuming one one's using it today...
+for v in 0.1.1 0.1.2 0.2.0 0.2.1; do
   git checkout kube-node-init-$v
-  helm dep build charts/kube-node-init
-  helm package charts/kube-node-init --destination packages
+  pushd charts/kube-node-init
+  CHART=.
+  BASE_DIR=../..
+  helm dep build ${CHART}
+  cat ${CHART}/requirements.*
+  ls -lah ${CHART}/charts
+  helm package ${CHART} --destination ${BASE_DIR}/packages
+  popd
 done
+
 git checkout master
